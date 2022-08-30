2fda15d8618276fc2289fed2d3b2435db982aa1f
Pedro Serer <42715517+Pedro-Serer@users.noreply.github.com>
2022-08-30 17:22:47 -0300

    Update Second-Hands.php

--- Second-Hands.php
+++ Second-Hands.php
@@ -1,20 +1,17 @@
 <?php
   
-	function test_1 ($n, $k, $vetor): string
-	{
-	  $qtd = max(array_count_values($vetor));
+    function test_1 ($n, $k, $vetor): string
+    {
+        $qtd = max(array_count_values($vetor));
 		
-		if ($n > ($k * 2) || ($qtd > 2)) return "NO";
-		
-		return "YES";
-	}
+        if ($n > ($k * 2) || ($qtd > 2)) return "NO";
+        return "YES";
+    }
   
-  
-  /*Testes*/
-  
-  $pointers = [[1, 2, 2], [1, 2, 3, 3, 1], [1, 2, 3, 4, 5], [1, 1, 2, 2, 1], [1]];
-	$boxies = [[3, 2], [5, 3], [5, 2],[5, 5], [1, 1]];
+    /*Testes*/
+    $pointers = [[1, 2, 2], [1, 2, 3, 3, 1], [1, 2, 3, 4, 5], [1, 1, 2, 2, 1], [1]];
+    $boxies = [[3, 2], [5, 3], [5, 2],[5, 5], [1, 1]];
 	
-	for ($i=0; $i<5; $i++) {
-		echo test_1($boxies[$i][0], $boxies[$i][1], $pointers[$i]) . "\n";
-	}
+    for ($i=0; $i<5; $i++) {
+        echo test_1($boxies[$i][0], $boxies[$i][1], $pointers[$i]) . "\n";
+    }
